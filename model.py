2b8254fd0cdb3245078d15dab6d00f5ab114552b
Hamid Taheri <taheri.hamiid@gmail.com>
2025-06-28 22:41:22 +0200

    input transforamtion is added for  invariant issue

---
+++ model.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+  
+class TNet(nn.Module):
+    def __init__(self, k=3):
+        super(TNet, self).__init__()
+        self.k = k
+        
+        self.conv1 = nn.Conv1d(k, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 1024, 1)
+        
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k * k)
+        
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+        
+        self.dropout = nn.Dropout(p=0.3)
+        
+    def forward(self, x):
+        batchsize = x.size()[0]
+        
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+        
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.dropout(self.fc2(x))))
+        x = self.fc3(x)
+        
+        iden = torch.eye(self.k, dtype=x.dtype, device=x.device).view(1, self.k * self.k).repeat(batchsize, 1)
+        x = x + iden
+        x = x.view(-1, self.k, self.k)
+        return x
+        
+        
+        
+        
+
+class PointNetEncoder(nn.Module):
+    def __init__(self, global_feat=True, feature_transform=False):
+        super(PointNetEncoder, self).__init__()
+        self.stn = TNet(k=3)
+        
+
+        self.global_feat = global_feat
+
+    def forward(self, x):
+        n_pts = x.size()[2]
+
+        trans = self.stn(x)
+
+
+
+class PointNetClassifier(nn.Module):
+    def __init__(self, k=10, feature_transform=False):
+        super(PointNetClassifier, self).__init__()
+        self.feature_transform = feature_transform
+        self.feat = PointNetEncoder(global_feat=True, feature_transform=feature_transform)
+        
+
+    def forward(self, x):
+        x, trans, trans_feat = self.feat(x)
+        
