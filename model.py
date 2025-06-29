062431c06ecbbb0e487a4e0ce3cf2d8feee1762e
Hamid Taheri <taheri.hamiid@gmail.com>
2025-06-29 14:08:55 +0200

    segmentation network is added at the end of the pointnet network

--- model.py
+++ model.py
@@ -134,6 +134,29 @@ class PointNetClassifier(nn.Module):
         return F.log_softmax(x, dim=1), trans, trans_feat
         
         
-        
-        
-        
+class PointNetSegmentation(nn.Module):
+    def __init__(self, k=2, feature_transform=False):
+        super(PointNetSegmentation, self).__init__()
+        self.k = k
+        self.feature_transform = feature_transform
+        self.feat = PointNetEncoder(global_feat=False, feature_transform=feature_transform)
+        self.conv1 = nn.Conv1d(1088, 512, 1)
+        self.conv2 = nn.Conv1d(512, 256, 1)
+        self.conv3 = nn.Conv1d(256, 128, 1)
+        self.conv4 = nn.Conv1d(128, k, 1)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.bn3 = nn.BatchNorm1d(128)
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        n_pts = x.size()[2]
+        x, trans, trans_feat = self.feat(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.conv4(x)
+        x = x.transpose(2, 1).contiguous()
+        x = F.log_softmax(x.view(-1, self.k), dim=-1)
+        x = x.view(batchsize, n_pts, self.k)
+        return x, trans, trans_feat
